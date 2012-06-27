0ca3bd2d8b0c73f1c63ebbb919db4486b2492acc
Chris Goedhart <goedhartchris@quicknet.nl>
2012-06-27 17:01:40 +0200

    Config.py added

---
+++ source/config.py
@@ -0,0 +1,12 @@
+# From which directory should UFORIA start to scan?
+# Example: startScan = '/home/user/' 
+startScan = '/home/carlo/public/openbaar/TESTBESTANDEN' 
+
+# How many Filepath Scanners should be active at the same time?
+# Example: numberOfScanners = 2
+numberOfScanners = 1
+
+# How many File Workers should be active at the same time?
+# Example: numberOfWorkers = 2
+numberOfWorkers = 1
+
