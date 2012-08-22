3c5b530474296857930bf0a1e84f850c427a9caf
Bart van der Wal <bwal@nikhef.nl>
2012-08-22 21:31:29 +0200

    Repo cleanup
    I have cleaned the repo so that there is only one version left. This it
    the final version given to me by John with the database bug fix.

--- source/hash.py
+++ source/hash.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
 import hashlib
-#Get file md5
+
+#This part of code will calculate the MD5 of a file
 def filetomd5(filepath):
 	md5 = hashlib.md5()
 	with open(filepath,'rb') as f: 
 		for chunk in iter(lambda: f.read(8192), b''): 
 			md5.update(chunk)
 	return md5.hexdigest() 
-#Get file sha1
+
+#This part of code will calculate the SHA1 of a file
 def filetosha1(filepath):
 	sha1 = hashlib.sha1()
 	with open(filepath,'rb') as f: 
@@ -16,7 +18,7 @@ def filetosha1(filepath):
 			sha1.update(chunk)
 	return sha1.hexdigest() 
 	
-#get file sha256
+#This part of code will calculate the SHA256 of a file
 def filetosha256(filepath):
 	sha256= hashlib.sha256()
 	with open(filepath,'rb') as f: 
