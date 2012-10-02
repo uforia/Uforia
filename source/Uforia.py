466ee358e3066e3174d834676fa438e4d00972b6
Arnim Eijkhoudt <a.eijkhoudt@hva.nl>
2012-10-02 15:18:28 +0200

    The old modulescanner is no longer needed. Module detection is now built-in and done initially on startup. An internal dict(ionary) is kept that directly maps MIME-types and the contents of the modules/ directory. The filescanner can then easily trigger the appropriate modules for each of the found files, by simply calling all files in 'modules/MIMETYPE-MAJOR/MIMETYPE-MINOR/...'

--- source/Uforia.py
+++ source/Uforia.py
@@ -5,7 +5,7 @@ import os, sys, re, time, multiprocessing
 sys.path.append('./include')
 
 # Load Uforia custom modules
-import config, File, magic, modscanner
+import config, File, magic
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Uforia.settings.settings")
 
 from Uforia.models import * 
@@ -13,55 +13,48 @@ from Uforia.modules.models import *
 
 class Uforia(object):
     def __init__(self):
+        print "Listing available modules for MIME types..."
+        self.moduleScanner()
         print "Setting up "+str(config.CONSUMERS)+" consumer(s)..."
         self.consumers = multiprocessing.Pool(processes=config.CONSUMERS)
         if config.DEBUG:
             print "Starting producer..."
         self.fileScanner(config.STARTDIR)
 
+    def moduleScanner(self):
+        if config.DEBUG:
+            print "Starting in directory "+config.MODULES+"..."
+            print "Starting modulescanner..."
+        self.modules={}
+        for major in os.listdir(config.MODULES):
+            for minor in os.listdir(config.MODULES+major):
+                mimetype=major+'/'+minor
+                self.modules[mimetype]=os.listdir(config.MODULES+mimetype)
+        if config.DEBUG:
+            print "Supported modules: "
+            for mimetype in self.modules:
+                print "Mimetype: "+mimetype+"\tList of handlers:",self.modules[mimetype]
+
+
     def fileScanner(self,dir):
         if config.DEBUG:
             print "Starting in directory "+config.STARTDIR+"..."
-            print "Starting Filepath Scanner..."
+            print "Starting filescanner..."
         for root, dirs, files in os.walk(dir, topdown=True, followlinks=False):
             for name in files:
                 fullpath=os.path.join(root,name)
                 self.consumers.apply_async(self.fileProcessor(fullpath))
                 if config.DEBUG:
                     print "Added:",fullpath
-        return
     
     def fileProcessor(self,fullpath):
         self.file = File.File(fullpath,config.DEBUG)
         try:
-	        hash.save()
 	        if config.DEBUG:
 	            print "Hashes and metadata exported to database."
-	        metadata = Metadata(Location = self.file.fullpath, Name = self.file.name, MTimes = self.file.mtime, ATimes = self.file.atime, CTimes = self.file.ctime, Owner = self.file.owner, Groups = self.file.group, Permissions = '?')
-	        metadata.save()
+	        # TODO: Store the stuff in the database
         except:
-            if not config.DEBUG:
-                raise IOError('Error storing hashes, possible database problem.')
-    
-        xmtype = ''.join(e for e in self.file.mtype if e.isalnum())
-        if config.DEBUG:
-            print "\n","Clean Filetype: ", xmtype
-        externalresult = xmtype
-        try:
-            result = modscanner.modname.index(externalresult)
-            rt = result + 1
-            resulttest = modscanner.modname[rt]
-            er = "modules/"
-            er += externalresult
-            if externalresult in resulttest:
-                filelauncher = resulttest
-                filelauncher += " "
-                filelauncher += filepath
-                os.system(filelauncher)
-        except:
-            pass
-        return
+            raise IOError('Error storing hashes, possible database problem.')
 					
 if __name__ == "__main__":
-    modscanner.findmods(modscanner.modtop)
     main = Uforia()
--- source/include/config.py
+++ source/include/config.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
-# From which directory should Uforia start to scan?
-# Example: startScan = '/home/user/' 
-STARTDIR = '/home/FILES/'
-
-# How many File Workers should be active at the same time?
-# Example: numberOfWorkers = 2
-CONSUMERS = 1
-
-# Enable debug code True or False
+# Enable debug output. Prints lots of information about internal workings.
+# Note: Debug output is very noisy and should not be used in production.
 DEBUG = True
 
+# From which directory should Uforia start to scan?
+# Example: STARTDIR = '/home/user/' 
+STARTDIR = '/home/FILES/'
 
+# Where should Uforia find the modules to handle found MIME types?
+# Note: You should not normally have to modify this!
+# Example: MODULES = './modules/'
+MODULES = './modules/'
+
+# How many processes should be parsing files at the same time?
+# Note: This is a tradeoff between CPU power and disk I/O. More workers
+# means higher CPU utilization, but disk I/O might become a limiting factor.
+# Conversely: less workers might mean optimal disk I/O, but your CPU might
+# be sitting idle...
+# Example: CONSUMERS = 2
+CONSUMERS = 1
