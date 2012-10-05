f383caaef40ce4b8f4fdc78001350836f9d7ba20
Arnim Eijkhoudt <a.eijkhoudt@hva.nl>
2012-10-05 11:31:32 +0200

    - Databases storage preparations
    Final decision is that Django is no longer needed.
    Removal of old support libraries
    Updated help files to reflect new file layout/configuration.

--- source/manage.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os,sys
-
-#If the executed file is the main file, the main file will use the settings from Django, imported from the following location:
-if __name__ == "__main__":
-	 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Uforia.settings.settings")
-	 from django.core.management import execute_from_command_line
-	 execute_from_command_line(sys.argv)
