5e701d97a5d39eea0035d6dda1640a7b96f1d98f
Arnim Eijkhoudt <a.eijkhoudt@hva.nl>
2012-10-16 17:06:35 +0200

    - Multithreading code completely reworked and actually works now ;-)

--- source/Uforia.py
+++ source/Uforia.py
@@ -10,103 +10,104 @@ sys.path.append('./databases')
 # Load Uforia custom modules
 import config, File, magic
 
-class Uforia(object):
-    def __init__(self):
+def run():
+    if config.DEBUG:
+        print("Initializing "+config.DBTYPE+" database connection...")
+    databaseModule = imp.load_source(config.DBTYPE,config.DATABASEDIR+config.DBTYPE+".py")
+    global db
+    db = databaseModule.Database(config)
+    if config.ENABLEMODULES:
         if config.DEBUG:
-            print("Initializing "+config.DBTYPE+" database connection...")
-        self.databaseModule = imp.load_source(config.DBTYPE,config.DATABASEDIR+config.DBTYPE+".py")
-        self.db = self.databaseModule.Database(config)
-        if config.ENABLEMODULES:
-            if config.DEBUG:
-                print("Listing available modules for MIME types...")
-            self.moduleScanner()
-        if config.DEBUG:
-            print("Setting up "+str(config.CONSUMERS)+" consumer(s)...")
-        self.consumers=multiprocessing.Pool(processes=config.CONSUMERS)
-        if config.DEBUG:
-            print("Starting producer...")
-        self.fileScanner(config.STARTDIR)
+            print("Listing available modules for MIME types...")
+        moduleScanner()
+    if config.DEBUG:
+        print("Setting up "+str(config.CONSUMERS)+" consumer(s)...")
+    consumers=multiprocessing.Pool(processes=config.CONSUMERS)
+    if config.DEBUG:
+        print("Starting producer...")
+    fileScanner(config.STARTDIR,consumers)
 
-    def moduleScanner(self):
-        if config.DEBUG:
-            print("Starting in directory "+config.MODULEDIR+"...")
-        self.modules={}
-        self.modulelist={}
-        self.tabletocolumns={}
-        self.moduletotable={}
-        for major in os.listdir(config.MODULEDIR):
-            for minor in os.listdir(config.MODULEDIR+major):
-                mimetype=major+'/'+minor
-                self.modulelist[mimetype]=glob.glob(config.MODULEDIR+mimetype+"/*.py")
-                for modulepath in self.modulelist[mimetype]:
-                    with open(modulepath) as file:
-                        for line in file:
-                            if line.startswith("""#TABLE: """):
-                                columns=line.strip('\n').replace("""#TABLE: """,'')
-                                modulename=modulepath[2:].strip(config.MODULEDIR).strip('.py').replace('/','.')
-                                tablename=modulepath[2:].strip(config.MODULEDIR).strip('.py').replace('/','_')
-                                self.modules[modulename]=imp.load_source(modulename,modulepath)
-                                self.moduletotable[modulename]=tablename
-                                self.tabletocolumns[tablename]=columns.split(':')[0]
-                                self.db.setup(self.moduletotable[modulename],columns)
+def moduleScanner():
+    if config.DEBUG:
+       print("Starting in directory "+config.MODULEDIR+"...")
+    modules={}
+    modulelist={}
+    tabletocolumns={}
+    moduletotable={}
+    for major in os.listdir(config.MODULEDIR):
+        for minor in os.listdir(config.MODULEDIR+major):
+            mimetype=major+'/'+minor
+            modulelist[mimetype]=glob.glob(config.MODULEDIR+mimetype+"/*.py")
+            for modulepath in modulelist[mimetype]:
+                with open(modulepath) as file:
+                    for line in file:
+                        if line.startswith("""#TABLE: """):
+                            columns=line.strip('\n').replace("""#TABLE: """,'')
+                            modulename=modulepath[2:].strip(config.MODULEDIR).strip('.py').replace('/','.')
+                            tablename=modulepath[2:].strip(config.MODULEDIR).strip('.py').replace('/','_')
+                            modules[modulename]=imp.load_source(modulename,modulepath)
+                            moduletotable[modulename]=tablename
+                            tabletocolumns[tablename]=columns.split(':')[0]
+                            db.setup(moduletotable[modulename],columns)
 
-    def fileScanner(self,dir):
+def fileScanner(dir,consumers):
+    if config.DEBUG:
+        print("Starting in directory "+config.STARTDIR+"...")
+        print("Starting filescanner...")
+    hashid=1
+    filelist=[]
+    for root, dirs, files in os.walk(dir, topdown=True, followlinks=False):
+        totalfiles=len(files)
+        for name in files:
+            fullpath=os.path.join(root,name)
+            filelist.append((fullpath,hashid,totalfiles))
+            hashid+=1;
+    consumers.map(fileProcessor, filelist)
+
+def fileProcessor(item):
+    fullpath=item[0]
+    hashid=item[1]
+    totalfiles=item[2]
+    file=File.File(fullpath,config)
+    if config.OUTPUT:
+        stdscr.addstr(0,0,"=== Uforia ===")
+        stdscr.addstr(2,0,"Examining:\t"+str(fullpath))
+        stdscr.clrtoeol()
+        stdscr.refresh()
+    try:
         if config.DEBUG:
-            print("Starting in directory "+config.STARTDIR+"...")
-            print("Starting filescanner...")
-        self.hashid=1
-        for root, dirs, files in os.walk(dir, topdown=True, followlinks=False):
-            totalfiles=float(len(files))
-            for name in files:
-                fullpath=os.path.join(root,name)
-                if config.OUTPUT:
-                    stdscr.addstr(0,0,"=== Uforia ===")
-                    stdscr.addstr(2,0,"Examining:\t"+str(fullpath))
-                    stdscr.clrtoeol()
-                    currentfile=float(files.index(name))
-                    percentage=round(100*(currentfile/totalfiles),1)
-                    stdscr.addstr(3,0,"Progress:\t"+str(percentage)+"%")
-                    stdscr.clrtoeol()
-                    stdscr.refresh()
-                self.consumers.apply_async(self.fileProcessor(fullpath,self.hashid))
-                self.hashid+=1;
-    
-    def fileProcessor(self,fullpath,hashid):
-        self.file=File.File(fullpath,config)
-        try:
-	        if config.DEBUG:
-	            print("Exporting basic hashes and metadata to database.")
-	        columns=('hashid','fullpath', 'name', 'size', 'owner', 'group', 'perm', 'mtime', 'atime', 'ctime', 'md5', 'sha1', 'sha256', 'ftype', 'mtype', 'btype')
-	        values=(hashid,self.file.fullpath, self.file.name, self.file.size, self.file.owner, self.file.group, self.file.perm, self.file.mtime, self.file.atime, self.file.ctime, self.file.md5, self.file.sha1, self.file.sha256, self.file.ftype, self.file.mtype, self.file.btype)
-	        self.db.store('files','',columns,values)
-        except:
-            raise
-        if not config.ENABLEMODULES:
+            print("Exporting basic hashes and metadata to database.")
+        columns=('hashid','fullpath', 'name', 'size', 'owner', 'group', 'perm', 'mtime', 'atime', 'ctime', 'md5', 'sha1', 'sha256', 'ftype', 'mtype', 'btype')
+        values=(hashid,file.fullpath, file.name, file.size, file.owner, file.group, file.perm, file.mtime, file.atime, file.ctime, file.md5, file.sha1, file.sha256, file.ftype, file.mtype, file.btype)
+        db.store('files','',columns,values)
+    except:
+        raise
+    if not config.ENABLEMODULES:
+        if config.DEBUG:
+            print("Additional module parsing disabled by config, skipping...")
+    else:
+        if file.mtype not in modulelist:
             if config.DEBUG:
-                print("Additional module parsing disabled by config, skipping...")
+                print("No modules found to handle MIME-type "+file.mtype+", skipping additional file parsing...")
         else:
-            if self.file.mtype not in self.modulelist:
-                if config.DEBUG:
-                    print("No modules found to handle MIME-type "+self.file.mtype+", skipping additional file parsing...")
-            else:
-       	        lasthashid=self.db.findhash((self.file.fullpath,))
+   	        lasthashid=db.findhash((file.fullpath,))
     	        if not lasthashid:
 	                raise
-                try:
-                    if config.DEBUG:
-                        print("Setting up "+str(config.MODULES)+" module workers...")
-                    self.modulepool=multiprocessing.Pool(processes=config.MODULES)
-                    handlers=[]
-                    for handler in self.modulelist[self.file.mtype]:
-                        handlers.append(handler[2:].strip(config.MODULEDIR).strip('.py').replace('/','.'))
-                    for s in handlers:
-                        func=getattr(self.modules[s],'process')
-                        table=self.moduletotable[s]
-                        columns=self.tabletocolumns[self.moduletotable[s]]
-                        args=(self.db,table,lasthashid,columns,self.file.fullpath)
-                        x=self.modulepool.apply_async(func(*args))
-                except:
-                    raise
+        try:
+            if config.DEBUG:
+                print("Setting up "+str(config.MODULES)+" module workers...")
+            modulepool=multiprocessing.Pool(processes=config.MODULES)
+            handlers=[]
+            for handler in modulelist[file.mtype]:
+                handlers.append(handler[2:].strip(config.MODULEDIR).strip('.py').replace('/','.'))
+            for s in handlers:
+                func=getattr(modules[s],'process')
+                table=moduletotable[s]
+                columns=tabletocolumns[moduletotable[s]]
+                args=(db,table,lasthashid,columns,file.fullpath)
+                x=modulepool.apply_async(func(*args))
+        except:
+            raise
 					
 if __name__ == "__main__":
     try:
@@ -115,7 +116,7 @@ if __name__ == "__main__":
             curses.noecho()
             curses.cbreak()
             stdscr.keypad(1)
-        main=Uforia()
+        run()
     finally:
         if config.OUTPUT:
             curses.nocbreak()
