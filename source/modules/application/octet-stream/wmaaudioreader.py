b8ecc380fd97c4d735390d002ddce410c7ec6c43
Jimmy van den Berg <jimmyvdberg@live.nl>
2013-03-08 21:09:01 +0100

    Code Review WMA
    Refactored to new module style

--- source/modules/application/octet-stream/wmaaudioreader.py
+++ source/modules/application/octet-stream/wmaaudioreader.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 
-# This is the module for audio
+# This is the audio module for WMA
 
-#TABLE: ARTIST:LONGTEXT, TITLE:LONGTEXT, CHANNELS:LONGTEXT, COMMENT:LONGTEXT, DURATION:INT(3), GENRE:LONGTEXT, SAMPLERATE:INT(3), SIZE:INT(3), TRACK:INT(3), VALID:LONGTEXT, YEAR:INT(3), ALBUM:LONGTEXT, AUDIO_SIZE:INT(3)
+#TABLE: ARTIST:LONGTEXT, TITLE:LONGTEXT, CHANNELS:LONGTEXT, COMMENT:LONGTEXT, DURATION:INT, GENRE:LONGTEXT, SAMPLERATE:INT, SIZE:INT, TRACK:INT(3), YEAR:INT(4), ALBUM:LONGTEXT, AUDIO_SIZE:INT
 
 # import for external lib hsaudiotag
-# This lib is used for the audio module, so please install it with the following command: pip install eyeD3
 from hsaudiotag import wma
-import imp
-import sys
+import imp, sys
 
 # Load Uforia custom modules
 try:
     config      = imp.load_source('config','include/config.py')
 except:
     raise
-
 
 def process(fullpath, columns=None):
     try:
@@ -24,35 +21,43 @@ def process(fullpath, columns=None):
         
         # If the wma file can be read succesfully
         if myfile.valid:
+            
+            assorted = [
+              myfile.artist,
+              myfile.title,
+              myfile.channels,
+              myfile.comment,
+              myfile.duration,
+              myfile.genre,
+              myfile.sample_rate,
+              myfile.size,
+              myfile.track,
+              myfile.year,
+              myfile.album,
+              myfile.audio_size
+            ]
+                 
+            # Make sure we stored exactly the same amount of columns as
+            # specified!!
+            assert len(assorted) == len(columns)
+            
+            # Print some data that is stored in the database if debug is true
+            if config.DEBUG:
+                print "\nWMA file data:"
+                for i in range(0, len(assorted)):
+                    print "%-18s %s" % (columns[i]+':', assorted[i])
+                print
+            
+            # Store in database  
+            return assorted 
         
-        # if debugging show this data
-             if config.DEBUG:
-                 print "\nAudio file data:"
-                 print "Artist:        %s" % myfile.artist
-                 print "Title:         %s" % myfile.title
-                 print "Channels:      %s" % myfile.channels
-                 print "Comment:       %s" % myfile.comment
-                 print "Duration:      %s" % myfile.duration
-                 print "Genre:         %s" % myfile.genre
-                 print "Sample Rate:   %s" % myfile.sample_rate
-                 print "Size:          %s" % myfile.size
-                 print "Track:         %s" % myfile.track
-                 print "Valid:         %s" % myfile.valid
-                 print "Year:          %s" % myfile.year
-                 print "Album:         %s" % myfile.album
-                 print "Audio Size:    %s" % myfile.audio_size
-#                print "Bitrate:       %s" % str(myfile.bitrate())
-                 print
-             
-             # Store in database    
-             return (myfile.artist, myfile.title, myfile.channels,  myfile.comment,  myfile.duration,  myfile.genre,  myfile.sample_rate,  myfile.size, myfile.track, myfile.valid, myfile.year, myfile.album, myfile.audio_size) 
         else:
-             return(None, None, None, None, None, None, None, None, None, None, None, None, None)
+             return None
     
     except:
         print "An error occured while parsing audio data: ", sys.exc_info()
         
         # Store nothing so the application won't crash
-        return (None, None, None, None, None, None, None, None, None, None, None, None, None)
+        return None
     
        
