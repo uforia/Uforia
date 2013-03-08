5c38fd03746393683925da649637ed528ebc0e0b
Ramon Vloon <ramonvloon@hotmail.com>
2013-03-08 15:25:46 +0100

    Wma file more checks
    Signed-off-by: Ramon Vloon <ramonvloon@hotmail.com>

--- source/modules/application/octet-stream/wmaaudioreader.py
+++ source/modules/application/octet-stream/wmaaudioreader.py
@@ -19,28 +19,40 @@ except:
 
 def process(fullpath, columns=None):
     try:
+        # Gets the WMA file from the path
         myfile = wma.WMADecoder(fullpath)
-        if config.DEBUG:
-            print "\nAudio file data:"
-            print "Artist:        %s" % myfile.artist
-            print "Title:         %s" % myfile.title
-            print "Channels:      %s" % myfile.channels
-            print "Comment:       %s" % myfile.comment
-            print "Duration:      %s" % myfile.duration
-            print "Genre:         %s" % myfile.genre
-            print "Sample Rate:   %s" % myfile.sample_rate
-            print "Size:          %s" % myfile.size
-            print "Track:         %s" % myfile.track
-            print "Valid:         %s" % myfile.valid
-            print "Year:          %s" % myfile.year
-            print "Album:         %s" % myfile.album
-            print "Audio Size:    %s" % myfile.audio_size
-#            print "Bitrate:       %s" % str(myfile.bitrate())
-            print
-
+        
+        # If the wma file can be read succesfully
+        if myfile.valid:
+        
+        # if debugging show this data
+             if config.DEBUG:
+                 print "\nAudio file data:"
+                 print "Artist:        %s" % myfile.artist
+                 print "Title:         %s" % myfile.title
+                 print "Channels:      %s" % myfile.channels
+                 print "Comment:       %s" % myfile.comment
+                 print "Duration:      %s" % myfile.duration
+                 print "Genre:         %s" % myfile.genre
+                 print "Sample Rate:   %s" % myfile.sample_rate
+                 print "Size:          %s" % myfile.size
+                 print "Track:         %s" % myfile.track
+                 print "Valid:         %s" % myfile.valid
+                 print "Year:          %s" % myfile.year
+                 print "Album:         %s" % myfile.album
+                 print "Audio Size:    %s" % myfile.audio_size
+#                print "Bitrate:       %s" % str(myfile.bitrate())
+                 print
+             
+             # Store in database    
+             return (myfile.artist, myfile.title, myfile.channels,  myfile.comment,  myfile.duration,  myfile.genre,  myfile.sample_rate,  myfile.size, myfile.track, myfile.valid, myfile.year, myfile.album, myfile.audio_size) 
+        else:
+             return(None, None, None, None, None, None, None, None, None, None, None, None, None)
     
     except:
         print "An error occured while parsing audio data: ", sys.exc_info()
         
-    return (myfile.artist, myfile.title, myfile.channels,  myfile.comment,  myfile.duration,  myfile.genre,  myfile.sample_rate,  myfile.size, myfile.track, myfile.valid, myfile.year, myfile.album, myfile.audio_size)
+        # Store nothing so the application won't crash
+        return (None, None, None, None, None, None, None, None, None, None, None, None, None)
+    
        
