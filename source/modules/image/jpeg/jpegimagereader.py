e413c1c87f40c3ccf7ef6c04d75e94633cd6ef4d
ramonvloon <ramonvloon@hotmail.com>
2013-03-19 13:12:19 +0100

    Documentation JPEG module added
    Signed-off-by: ramonvloon <ramonvloon@hotmail.com>

--- source/modules/image/jpeg/jpegimagereader.py
+++ source/modules/image/jpeg/jpegimagereader.py
@@ -13,30 +13,30 @@ try:
 except:
     raise
 
-def process(fullpath, columns=None):
-        # Try to parse JPG data        
+def process(fullpath, columns=None):     
         try:
-            #raise
+            # Try to parse JPG data   
             image = Image.open(fullpath, "r")
             
+            # Create an empty list
             assorted = []
             info_dictionary = image.info
             
-            # Check if ICC profile is in info dictionary, if so put it in our list
+            # Check if jfif is in info dictionary, if so put it in our list
             if "jfif" in info_dictionary:
                 assorted.append(info_dictionary["jfif"])
                 info_dictionary.pop("jfif")
             else:
                 assorted.append(None)
             
-            # Check if interlace is in info dictionary, if so put it in our list
+            # Check if adobe is in info dictionary, if so put it in our list
             if "adobe" in info_dictionary:
                 assorted.append(info_dictionary["adobe"])
                 info_dictionary.pop("adobe")
             else:
                 assorted.append(None)
             
-            # Check if transparency is in info dictionary, if so put it in our list    
+            # Check if progression is in info dictionary, if so put it in our list    
             if "progression" in info_dictionary:
                 assorted.append(info_dictionary["progression"])
                 info_dictionary.pop("progression")
@@ -44,20 +44,20 @@ def process(fullpath, columns=None):
                 assorted.append(None)
             
             # Check if gamma is in info dictionary, if so put it in our list      
-            if "quality" in info_dictionary:
+            if "gamma" in info_dictionary:
                 assorted.append(info_dictionary["quality"])
                 info_dictionary.pop("quality")
             else:
                 assorted.append(None)
             
-            # Check if dpi is in info dictionary, if so put it in our list     
+            # Check if optimize is in info dictionary, if so put it in our list     
             if "optimize" in info_dictionary:
                 assorted.append(info_dictionary["optimize"][0])
                 info_dictionary.pop("optimize")
             else:
                 assorted.append(None)
             
-            # Check if aspect is in info dictionary, if so put it in our list                     
+            # Check if exif is in info dictionary, if so decode and put in our list                    
             if "exif" in info_dictionary:
                 exif = {
                         TAGS[k]: v
@@ -68,37 +68,43 @@ def process(fullpath, columns=None):
                 info_dictionary.pop("exif")
             else:
                 assorted.append(None)
-                
+      
+            # Check if jfif_version is in info dictionary, if so put it in our list       
             if "jfif_version" in image.info:
                 assorted.append(info_dictionary["jfif_version"])
                 info_dictionary.pop("jfif_version")
             else:
                 assorted.append(None)    
                 
+            # Check if jfif_density is in info dictionary, if so put it in our list       
             if "jfif_density" in image.info:
                 assorted.append(info_dictionary["jfif_density"])
                 info_dictionary.pop("jfif_density")
             else:
                 assorted.append(None)
-                
+
+            # Check if jfif_unit is in info dictionary, if so put it in our list                      
             if "jfif_unit" in image.info:
                 assorted.append(info_dictionary["jfif_unit"])
                 info_dictionary.pop("jfif_unit")
             else:
                 assorted.append(None)          
 
+            # Check if flashpix is in info dictionary, if so put it in our list       
             if "flashpix" in image.info:
                 assorted.append(info_dictionary["flashpix"])
                 info_dictionary.pop("flashpix")
             else:
                 assorted.append(None)  
 
+            # Check if adobe_transform is in info dictionary, if so put it in our list       
             if "adobe_transform" in image.info:
                 assorted.append(info_dictionary["adobe_transform"])
                 info_dictionary.pop("adobe_transform")
             else:
                 assorted.append(None)                                         
-                                      
+                 
+            # Check if dpi is in info dictionary, if so put it in our list                            
             if "dpi" in info_dictionary:
                 assorted.append(info_dictionary["dpi"][0])
                 assorted.append(info_dictionary["dpi"][1])
@@ -106,10 +112,7 @@ def process(fullpath, columns=None):
             else:
                 assorted.append(None)
                 assorted.append(None)             
-                
-            # If there are still other values in the dict then put those in column
-#            assorted.append(info_dictionary)
-            
+                            
             # Delete variable
             del info_dictionary, image
             
@@ -136,13 +139,3 @@ def process(fullpath, columns=None):
         
             # Store values in database so not the whole application crashes
             return None
-    
-# 
-#def get_exif(fn):
-#    ret = {}
-#    i = Image.open(fn)
-#    info = i._getexif()
-#    for tag, value in info.items():
-#        decoded = TAGS.get(tag, tag)
-#        ret[decoded] = value
-#    return ret
