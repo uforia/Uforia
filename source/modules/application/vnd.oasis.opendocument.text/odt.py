9054e02c2c0b8bf0f87179b49d628240b2a31164
Arnim Eijkhoudt <a.eijkhoudt@hva.nl>
2013-04-22 14:43:15 +0200

    Added BeautifulSoup v4 to the libraries. Fixed BeautifulSoup KeyError when parsing an empty dictionary item (odtdict bug).

--- source/modules/application/vnd.oasis.opendocument.text/odt.py
+++ source/modules/application/vnd.oasis.opendocument.text/odt.py
@@ -2,48 +2,44 @@
 
 #TABLE: wordCount:INT, imageCount:INT, objectCount:INT, pageCount:INT, charCount: INT, parCount: INT, tableCount: INT, content:TEXT 
 
-import re, zipfile, sys
-import BeautifulSoup
+import re, zipfile, sys, bs4
 
 
 def process(fullpath, config, columns=None):
-	try:
-		document = zipfile.ZipFile(fullpath)
-	except:
-		exit()
+    try:
+    	document = zipfile.ZipFile(fullpath)
+    except:
+    	exit()
 
 
-	#get content/text
-	xml = document.read("content.xml")
+    #get content/text
+    xml = document.read("content.xml")
 
-	#get metadata	
-	xmlmeta = document.read("meta.xml")
+    #get metadata	
+    xmlmeta = document.read("meta.xml")
 
-	# Finding content (Text)
-	maincontent = re.compile('<text:p([^>]*)>([^<]*)</text:p>')
-	listtotal = maincontent.findall(xml)
-	textcontent = ""
-	
-	#MSWord/odt splits every word into seperate xml tags. Adding them to a single string instead
-	for words in listtotal:		
-		textcontent = textcontent + "%s" % (words[1].rstrip('\n') )
+    # Finding content (Text)
+    maincontent = re.compile('<text:p([^>]*)>([^<]*)</text:p>')
+    listtotal = maincontent.findall(xml)
+    textcontent = ""
+    
+    #MSWord/odt splits every word into seperate xml tags. Adding them to a single string instead
+    for words in listtotal:		
+    	textcontent = textcontent + "%s" % (words[1].rstrip('\n') )
 
 
-	#Minidom alternative
-	xmlsoup = BeautifulSoup.BeautifulSoup(xmlmeta)
-	
-	odtdict = ""
-	for message in xmlsoup.findAll('meta:document-statistic'):
-		odtdict = dict(message.attrs)
-	
-	results = []
-	results.append(odtdict["meta:word-count"])
-	results.append(odtdict["meta:image-count"])
-	results.append(odtdict["meta:object-count"])
-	results.append(odtdict["meta:page-count"])
-	results.append(odtdict["meta:character-count"])
-	results.append(odtdict["meta:paragraph-count"])
-	results.append(odtdict["meta:table-count"])
-	results.append(textcontent)
+    #Minidom alternative
+    xmlsoup = bs4.BeautifulSoup(xmlmeta)
+    
+    odtdict = ""
+    for message in xmlsoup.findAll('meta:document-statistic'):
+    	odtdict = dict(message.attrs)
 
-	return results
+    results = []
+    for i in ('meta:word-count','meta:image-count','meta:row-count','meta:character-count','meta:non-whitespace-character-count','meta:paragraph-count','meta-page-count'):
+        try:
+            results.append(odtdict[i])
+        except KeyError:
+            results.append('')
+    results.append(textcontent)
+    return results
