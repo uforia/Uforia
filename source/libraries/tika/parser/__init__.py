fb975ee75a70514b2d3b4641e9eaa2d77eba4244
Joey Dreijer <joeydreijer@gmail.com>
2013-06-25 03:41:07 +0200

    Tika Support
    Added necessary (pre-compiled) .jar files to the tika dir. Includes
    parser, core and app. Pre-requirement is the installation of python JCC
    (apt-get install jcc). python setup.py build && python setup.py install
    afterwards (inside tika dir). This installs binding for tika (original
    source for binding inside readme)

---
+++ source/libraries/tika/parser/__init__.py
@@ -0,0 +1,32 @@
+import tika
+
+def from_file(filename):
+    """Parse filename using tika's AutoDetectParser."""
+    stream = tika.FileInputStream(tika.File(filename))
+    return __parse(stream)
+
+def from_buffer(string):
+    """Parse raw binary string using tika's AutoDetectParser."""
+    stream = tika.ByteArrayInputStream(tika.JArray_byte(string))
+    return __parse(stream)
+
+def __parse(stream):
+    if not tika.getVMEnv():
+        raise RuntimeError("tika.initVM() not called")
+    parsed = {}
+    parser = tika.AutoDetectParser()
+    content = tika.BodyContentHandler()
+    metadata = tika.Metadata()
+    context = tika.ParseContext()
+    parser.parse(stream, content, metadata, context)
+    parsed["content"] = content.toString()
+    parsed["metadata"] = {}
+    for n in metadata.names():
+        parsed["metadata"][n] = metadata.get(n)
+    return parsed
+    
+    
+    
+
+    
+    
