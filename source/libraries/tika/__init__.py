e162523e5ca76fea154f219053a0b1d61bd19104
Marcin Koziuk <marcin.koziuk@gmail.com>
2013-09-13 19:06:40 +0200

    Make Tika stand-alone; do not require installation. Also build with older glibc for compatibility. No binaries for Windows or Linux 32-bit available yet.

--- source/libraries/tika/__init__.py
+++ source/libraries/tika/__init__.py
@@ -1,23 +1,23 @@
-
 import os, _tika
 
+#_tika = libutil.load_library('tika', '_tika')
 __dir__ = os.path.abspath(os.path.dirname(__file__))
 
 class JavaError(Exception):
-    def getJavaException(self):
-        return self.args[0]
-    def __str__(self):
-        writer = StringWriter()
-        self.getJavaException().printStackTrace(PrintWriter(writer))
-        return "\n".join((super(JavaError, self).__str__(), "    Java stacktrace:", str(writer)))
+  def getJavaException(self):
+    return self.args[0]
+  def __str__(self):
+    writer = StringWriter()
+    self.getJavaException().printStackTrace(PrintWriter(writer))
+    return "\n".join((super(JavaError, self).__str__(), "    Java stacktrace:", str(writer)))
 
 class InvalidArgsError(Exception):
-    pass
+  pass
 
 _tika._set_exception_types(JavaError, InvalidArgsError)
 
 VERSION = "1.3"
-CLASSPATH = [os.path.join(__dir__, "tika-parsers-1.3.jar"), os.path.join(__dir__, "tika-core-1.3.jar"), os.path.join(__dir__, "org.eclipse.osgi.jar"), os.path.join(__dir__, "tika-app-1.3.jar"), os.path.join(__dir__, "log4j.properties.jar")]
+CLASSPATH = [os.path.join(__dir__, "org.eclipse.osgi.jar"), os.path.join(__dir__, "log4j.properties.jar"), os.path.join(__dir__, "tika-app-1.3.jar"), os.path.join(__dir__, "tika-parsers-1.3.jar"), os.path.join(__dir__, "tika-core-1.3.jar")]
 CLASSPATH = os.pathsep.join(CLASSPATH)
 _tika.CLASSPATH = CLASSPATH
 _tika._set_function_self(_tika.initVM, _tika)
--- source/libraries/tika/_tika.py
+++ source/libraries/tika/_tika.py
@@ -1,7 +1,9 @@
+import libutil
+
 def __bootstrap__():
    global __bootstrap__, __loader__, __file__
    import sys, pkg_resources, imp
-   __file__ = pkg_resources.resource_filename(__name__,'_tika.so')
+   __file__ = libutil.get_library_path('tika', '_tika')
    __loader__ = None; del __bootstrap__, __loader__
    imp.load_dynamic(__name__,__file__)
 __bootstrap__()
