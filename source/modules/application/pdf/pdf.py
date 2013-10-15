2da3f1a98020db7a50430b4338643829c906536c
Marcin Koziuk <marcin.koziuk@gmail.com>
2013-10-15 17:07:45 +0200

    Call Uforia recursively on extracted tika content
    NOTE: Tika doesn't extract PDF images
    TODO: move the code to a separate python file so the xls and msdoc
    modules can use it too

--- source/modules/application/pdf/pdf.py
+++ source/modules/application/pdf/pdf.py
@@ -18,26 +18,83 @@
 
 import string
 import tika
+import recursive
+import traceback
+import tempfile
+import shutil
+import os
+import sys
+import subprocess
 
+
+def _extractall(fullpath, tempdir):
+    # Path that leads to the archive
+    if fullpath is None:
+        raise Exception("Archive path not given")
+
+    # Path that leads to the destination
+    if tempdir is None:
+        raise Exception("Tempdir not given")
+
+    # Set tika location
+    tika_jar_path = os.path.abspath("./libraries/tika/tika-app-1.3.jar")
+    if not os.path.exists(tika_jar_path):
+        raise Exception("Tika not found at " + tika_jar_path)
+    
+    p = subprocess.Popen(["java",
+                          "-jar",
+                          tika_jar_path,
+                          "--extract",
+                          fullpath],
+                         cwd=tempdir)
+    output = p.communicate()[0]
+
+    if output is not None:
+        raise Exception("tika extract command failed") 
 
 def process(fullpath, config, rcontext, columns=None):
-	parser = tika.AutoDetectParser()
+    parser = tika.AutoDetectParser()
 
-	input = tika.FileInputStream(tika.File(fullpath))
+    input = tika.FileInputStream(tika.File(fullpath))
 
-	content = tika.BodyContentHandler()
-	metadata = tika.Metadata()
-	context = tika.ParseContext()
+    content = tika.BodyContentHandler()
+    metadata = tika.Metadata()
+    context = tika.ParseContext()
+    
+    parser.parse(input,content,metadata,context)
+    content = content.toString()
 
-	parser.parse(input,content,metadata,context)
-	content = content.toString()
+    processed = [
+        metadata.get("Creation-Date"),
+        metadata.get("Last-Modified"),
+        metadata.get("Last-Save-Date"),
+        metadata.get("Author"),
+        metadata.get("producer"),
+        metadata.get("xmpTPg:NPages"),
+        content
+    ]
 
-	return [
-		metadata.get("Creation-Date"),
-		metadata.get("Last-Modified"),
-		metadata.get("Last-Save-Date"),
-		metadata.get("Author"),
-		metadata.get("producer"),
-		metadata.get("xmpTPg:NPages"),
-		content
-	]
+    # To skip recursive call if there are no files to extract
+    extractor = tika.ParserContainerExtractor()
+    needs_extraction = extractor.isSupported(tika.TikaInputStream.get(input))
+
+    if needs_extraction:
+        # Call Uforia recursively on embedded files
+        tempdir = None
+        try:
+            # Perform extraction
+            tmpdir = tempfile.mkdtemp(dir=config.EXTRACTDIR)
+            _extractall(fullpath, tmpdir)
+
+            # Call Uforia again
+            recursive.call_uforia_recursive(config, rcontext, tmpdir, fullpath) 
+        except:
+            traceback.print_exc(file=sys.stderr)
+        finally:    
+            try:
+                if tempdir:
+                    shutil.rmtree(tmpdir)  # delete directory
+            except OSError as exc:
+                traceback.print_exc(file=sys.stderr)
+
+    return processed
