5e9db77739b09cdcc6c4e00775a0be9744d250f5
Jasper Timmer <jjwtimmer@gmail.com>
2014-10-05 20:42:40 +0200

    Rewrite of .eml module, extracts attachments now.

--- source/modules/application/octet-stream/pst.py
+++ source/modules/application/octet-stream/pst.py
@@ -1,45 +1,45 @@
 # TABLE: file_names:LONGTEXT, total_files:INT, zip_stored:INT, zip_deflated:INT, debug:LONGTEXT, comment:LONGTEXT, content_info:LONGTEXT
 
-import subprocess
 import shutil
-import traceback
+import subprocess
 import sys
 import tempfile
+import traceback
 
-import recursive
 import libutil
+import recursive
 
 
 def process(file, config, rcontext, columns=None):
     fullpath = file.fullpath
-    readpst_path = libutil.get_executable("libpst", "readpst")
+    if file.btype.startswith("Microsoft Outlook email folder"):
+        readpst_path = libutil.get_executable("libpst", "readpst")
 
-    tempdir = None
+        tempdir = None
 
-    try:
-        tempdir = tempfile.mkdtemp(dir=config.EXTRACTDIR)
-
-        p = subprocess.Popen([
-            readpst_path,
-            '-M',
-            '-o',
-            tempdir,
-            fullpath
-        ])
-
-        err = p.communicate()[1]
-
-        if err is not None:
-            raise Exception("readpst failed to extract " + fullpath)
-
-        recursive.call_uforia_recursive(config, rcontext, tempdir, fullpath)
-    except Exception as e:
-        traceback.print_exc(file=sys.stderr)
-    finally:
         try:
-            if tempdir:
-                shutil.rmtree(tempdir)  # delete directory
-        except OSError as exc:
-            traceback.print_exc(file=sys.stderr)
+            tempdir = tempfile.mkdtemp(dir=config.EXTRACTDIR)
 
-    return None
+            p = subprocess.Popen([
+                readpst_path,
+                '-e',
+                '-q',
+                '-o',
+                tempdir,
+                fullpath
+            ])
+
+            err = p.communicate()[1]
+
+            if err is not None:
+                raise Exception("readpst failed to extract " + fullpath)
+
+            recursive.call_uforia_recursive(config, rcontext, tempdir, fullpath)
+        except:
+            traceback.print_exc(file=sys.stderr)
+        finally:
+            try:
+                if tempdir:
+                    shutil.rmtree(tempdir)  # delete directory
+            except OSError as exc:
+                traceback.print_exc(file=sys.stderr)
--- source/modules/message/rfc822/rfc822filereader.py
+++ source/modules/message/rfc822/rfc822filereader.py
@@ -13,13 +13,16 @@
 # This is the module for handling rfc822 email types
 
 # Do not change from CamelCase because these are the official header names
-# TABLE: Delivered_To:LONGTEXT, Original_Recipient:LONGTEXT, Received:LONGTEXT, Return_Path:LONGTEXT, Received_SPF:LONGTEXT, Authentication_Results:LONGTEXT, DKIM_Signature:LONGTEXT, DomainKey_Signature:LONGTEXT, Organization:LONGTEXT, MIME_Version:DOUBLE, List_Unsubscribe:LONGTEXT, X_Received:LONGTEXT, X_Priority:LONGTEXT, X_MSMail_Priority:LONGTEXT, X_Mailer:LONGTEXT, X_MimeOLE:LONGTEXT, X_Notifications:LONGTEXT, X_Notification_ID:LONGTEXT, X_Sender_ID:LONGTEXT, X_Notification_Category:LONGTEXT, X_Notification_Type:LONGTEXT, X_UB:INT, Precedence:LONGTEXT, Reply_To:LONGTEXT, Auto_Submitted:LONGTEXT, Message_ID:LONGTEXT, Date:DATE, Subject:LONGTEXT, From:LONGTEXT, To:LONGTEXT, Content_Type:LONGTEXT, XTo:LONGTEXT, Xcc:LONGTEXT, Xbcc:LONGTEXT, Cc:LONGTEXT, Bcc:LONGTEXT, Email_Content:LONGTEXT
+# TABLE: Delivered_To:LONGTEXT, Original_Recipient:LONGTEXT, Received:LONGTEXT, Return_Path:LONGTEXT, Received_SPF:LONGTEXT, Authentication_Results:LONGTEXT, DKIM_Signature:LONGTEXT, DomainKey_Signature:LONGTEXT, Organization:LONGTEXT, MIME_Version:DOUBLE, List_Unsubscribe:LONGTEXT, X_Received:LONGTEXT, X_Priority:LONGTEXT, X_MSMail_Priority:LONGTEXT, X_Mailer:LONGTEXT, X_MimeOLE:LONGTEXT, X_Notifications:LONGTEXT, X_Notification_ID:LONGTEXT, X_Sender_ID:LONGTEXT, X_Notification_Category:LONGTEXT, X_Notification_Type:LONGTEXT, X_UB:INT, Precedence:LONGTEXT, Reply_To:LONGTEXT, Auto_Submitted:LONGTEXT, Message_ID:LONGTEXT, Date:DATE, Subject:LONGTEXT, From:LONGTEXT, To:LONGTEXT, Content_Type:LONGTEXT, XTo:LONGTEXT, Xcc:LONGTEXT, Xbcc:LONGTEXT, Cc:LONGTEXT, Bcc:LONGTEXT, Email_Content:LONGTEXT, Attachments:LONGTEXT
 
+import os
 import sys
 import traceback
-from email.parser import Parser
+import shutil
+import pyzmail
+import recursive
+import tempfile
 import python_dateutil.dateutil.parser as date_parser
-import re
 
 def process(file, config, rcontext, columns=None):
         fullpath = file.fullpath
@@ -27,68 +30,77 @@ def process(file, config, rcontext, columns=None):
         # Try to parse rfc822 data
         try:
             #  Get the e-mail headers from a file
-            email = open(fullpath, 'r')
-            headers = Parser().parse(email)
+            email_file = open(fullpath, 'r')
+            msg = pyzmail.PyzMessage.factory(email_file)
+
+            # find all attachments and save them to a temp folder
+            tempdir = None
+            attachments = []
+            try:
+                tempdir = tempfile.mkdtemp(dir=config.EXTRACTDIR)
+                for mailpart in msg.mailparts:
+
+                    if not mailpart.is_body:
+                        attachments.append(mailpart.filename)
+                        f = open(os.path.join(tempdir, mailpart.filename), 'wb')
+                        f.write(mailpart.get_payload())
+                        f.close()
+                if len(attachments) > 0:
+                    recursive.call_uforia_recursive(config, rcontext, tempdir, fullpath)
+            except:
+                traceback.print_exc(file=sys.stderr)
+            finally:
+                try:
+                    if tempdir:
+                        shutil.rmtree(tempdir)  # delete directory
+                except OSError as exc:
+                    traceback.print_exc(file=sys.stderr)
 
             # Get most common headers
-            try:
-                ReplyTo = re.sub(r'\s+',' ',headers["Reply-To"]).lstrip()
-            except:
-                ReplyTo = None
-            try:
-                To = re.sub(r'\s+',' ',headers["To"]).lstrip()
-            except:
-                To = None
-            try:
-                From = re.sub(r'\s+',' ',headers["From"]).lstrip()
-            except:
-                From = None
-            try:
-                XTo = re.sub(r'\s+',' ',headers["X-To"]).lstrip()
-            except:
-                XTo = None
-            try:
-                Xcc = re.sub(r'\s+',' ',headers["X-Cc"]).lstrip()
-            except:
-                Xcc = None
-            try:
-                Xbcc = re.sub(r'\s+',' ',headers["X-Bcc"]).lstrip()
-            except:
-                Xbcc = None
-            try:
-                Cc = re.sub(r'\s+',' ',headers["Cc"]).lstrip()
-            except:
-                Cc = None
-            try:
-                Bcc = re.sub(r'\s+',' ',headers["Bcc"]).lstrip()
-            except:
-                Bcc = None
-            assorted = [headers["Delivered-To"], headers["Original-Recipient"],
-                        headers["Received"], headers["Return-Path"],
-                        headers["Received-SPF"],
-                        headers["Authentication-Results"],
-                        headers["DKIM-Signature"],
-                        headers["DomainKey-Signature"],
-                        headers["Organization"], headers["MIME-Version"],
-                        headers["List-Unsubscribe"], headers["X-Received"],
-                        headers["X-Priority"], headers["X-MSMail-Priority"],
-                        headers["X-Mailer"], headers["X-MimeOLE"],
-                        headers["X-Notifications"],
-                        headers["X-Notification-ID"],
-                        headers["X-Sender-ID"],
-                        headers["X-Notification-Category"],
-                        headers["X-Notification-Type"], headers["X-UB"],
-                        headers["Precedence"], ReplyTo,
-                        headers["Auto-Submitted"], headers["Message-ID"],
-                        date_parser.parse(headers["Date"]),
-                        headers["Subject"], From, To, headers["Content-Type"],
-                        XTo, Xcc, Xbcc, Cc, Bcc]
+            assorted = [msg.get_decoded_header("Delivered-To", None),
+                        msg.get_decoded_header("Original-Recipient", None),
+                        msg.get_decoded_header("Received", None),
+                        msg.get_decoded_header("Return-Path", None),
+                        msg.get_decoded_header("Received-SPF", None),
+                        msg.get_decoded_header("Authentication-Results", None),
+                        msg.get_decoded_header("DKIM-Signature", None),
+                        msg.get_decoded_header("DomainKey-Signature", None),
+                        msg.get_decoded_header("Organization", None),
+                        msg.get_decoded_header("MIME-Version", None),
+                        msg.get_decoded_header("List-Unsubscribe", None),
+                        msg.get_decoded_header("X-Received", None),
+                        msg.get_decoded_header("X-Priority", None),
+                        msg.get_decoded_header("X-MSMail-Priority", None),
+                        msg.get_decoded_header("X-Mailer", None),
+                        msg.get_decoded_header("X-MimeOLE", None),
+                        msg.get_decoded_header("X-Notifications", None),
+                        msg.get_decoded_header("X-Notification-ID", None),
+                        msg.get_decoded_header("X-Sender-ID", None),
+                        msg.get_decoded_header("X-Notification-Category", None),
+                        msg.get_decoded_header("X-Notification-Type", None),
+                        msg.get_decoded_header("X-UB", None),
+                        msg.get_decoded_header("Precedence", None),
+                        msg.get_decoded_header("Reply-To", None),
+                        msg.get_decoded_header("Auto-Submitted", None),
+                        msg.get_decoded_header("Message-ID", None),
+                        date_parser.parse(msg.get_decoded_header("Date", None)),
+                        msg.get_decoded_header("Subject", None),
+                        msg.get_decoded_header("From", None),
+                        msg.get_decoded_header("To", None),
+                        msg.get_decoded_header("Content-Type", None),
+                        msg.get_decoded_header("X-To", None),
+                        msg.get_decoded_header("X-Cc", None),
+                        msg.get_decoded_header("X-Bcc", None),
+                        msg.get_decoded_header("Cc", None),
+                        msg.get_decoded_header("Bcc", None)]
 
             # Start at the beginning of the file
-            email.seek(0)
+            email_file.seek(0)
 
             # Put whole email file in database
-            assorted.append(email.read())
+            assorted.append(email_file.read())
+
+            assorted.append(','.join(attachments))
 
             # Make sure we stored exactly the same amount of columns as
             # specified!!
